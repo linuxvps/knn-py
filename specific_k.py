6f5e1c96b8bf07ad2743226150fd08ecfa737088
nima <linux.vps.vpn@gmail.com>
2024-05-10 16:27:42 +0330

    calc specific_k

--- specific_k.py
+++ specific_k.py
@@ -4,12 +4,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 
+
 def calc_score_for_specific_k(k):
     model = KNeighborsClassifier(n_neighbors=k)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     return accuracy
+def find_best_k(X_train, X_test, y_train, y_test):
+    max_accuracy = 0
+    best_k = 1
+
+    for k in range(1, 80):
+        accuracy = calc_score_for_specific_k(k)
+        if accuracy > max_accuracy:
+            max_accuracy = accuracy
+            best_k = k
+
+    return best_k, max_accuracy
 
 
 iris = load_iris()
@@ -18,6 +30,6 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-k = 80
-score = calc_score_for_specific_k(k)
-print(score)
+best_k, specific_k = find_best_k(X_train, X_test, y_train, y_test)
+print("Best K:", best_k)
+print("Accuracy:", specific_k)
